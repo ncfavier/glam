f713f2049c939d7691a0a27f47f90c87b82efac2
Naïm Favier <fnaim42@gmail.com>
2020-06-25 12:47:48 +0200

    www

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,29 @@
+{-# LANGUAGE CPP #-}
 module Main where
-
-import System.Console.Haskeline
 
 import Glam.Term
 import Glam.Parse
+
+#ifdef __GHCJS__
+
+import Data.JSString
+import GHCJS.Marshal
+import GHCJS.Foreign.Callback
+
+foreign import javascript unsafe "init($1)"
+    init_ :: Callback a -> IO ()
+
+main = do
+    eval <- syncCallback1' $ \v -> do
+        Just input <- fromJSVal v
+        toJSVal $ pack $ case parseOne term (unpack input) of
+            Right t -> show (normalise t)
+            Left e -> e
+    init_ eval
+
+#else
+
+import System.Console.Haskeline
 
 settings = defaultSettings { historyFile = Just ".glam" }
 
@@ -20,3 +40,5 @@ loop = do
                 Right t -> outputStrLn (show (normalise t))
                 Left e -> outputStr e
             loop
+
+#endif
